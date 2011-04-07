bd3f363ad60b2371768d84e6ac44160ee48088a0
Martin Hunt <mghunt@gmail.com>
2011-04-07 05:58:25 -0700

    on-screen logging for keyboard example with handling of up/repeat/down events

--- keyboard/entry/single.js
+++ keyboard/entry/single.js
@@ -1,33 +1,52 @@
 "use import";
 
-var app = new GCApp();
-var mainView = app.getView();
+import timestep.ui.Console;
 
-var keyListener = app.getKeyListener();
+var app = new GCApp(),
+	mainView = app.getView();
+
+var console = new timestep.ui.Console({x: 25, parent: mainView});
+logger.setListener(bind(console, 'log')); // where does logger go?
 
 mainView.tick = function(dt) {
 	// keyListener.popEvents will return an array of all events that 
 	// have happened since it was last called.
-	var keyEvents = keyListener.popEvents();
-	for (var i = 0; i < keyEvents.length; i++) {
-		var pressedKeys = keyListener.getPressed();
-		//TODO: log these
-		var event = keyEvents[i];
-		//event.code is the keycode of the key that raised the event
-		if (event.code == keyListener.LEFT) {
-			//if event.lifted is true, it means that the event is for
-			// a key being lifted.  Otherwise the key was pressed.
-			if (event.lifted) {
-				logger.log('left arrow up');
-			} else {
-				logger.log('left arrow down');
+	var keyListener = app.getKeyListener();
+	
+	// IMPORTANT: keyListener might not exist for some devices!
+	if (keyListener) {
+		keyEvents = keyListener.popEvents();
+	
+		// log the key up/down events
+		for (var i = 0; i < keyEvents.length; i++) {
+			var evt = keyEvents[i];
+			// evt.code is the keycode of the key that raised the event
+			if (evt.code == keyListener.LEFT) {
+				//if evt.lifted is true, it means that the evt is for
+				// a key being lifted.  Otherwise the key was pressed.
+				if (evt.lifted) {
+					logger.log('left arrow up');
+				} else {
+					logger.log('left arrow down');
+				}
+			} else if (evt.code == keyListener.RIGHT) {
+				if (evt.lifted) {
+					logger.log('right arrow up');
+				} else {
+					logger.log('right arrow down');
+				}
 			}
-		} else if (event.code == keyListener.RIGHT) {
-			if (event.lifted) {
-				logger.log('right arrow up');
-			} else {
-				logger.log('right arrow pressed');
-			}
+		}
+		
+		// check for keys held down
+		var pressed = keyListener.getPressed();
+		if (keyListener.LEFT in pressed) {
+			logger.log('left arrow pressed');
+		}
+		
+		if (keyListener.RIGHT in pressed) {
+			logger.log('right arrow pressed');
 		}
 	}
 }
+
