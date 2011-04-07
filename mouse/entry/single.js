dbfc6a0c78ea7feb16bc8938e583ae4e5e16266c
Martin Hunt <mghunt@gmail.com>
2011-04-07 05:57:27 -0700

    on-canvas logging of mouse events with some comments

--- mouse/entry/single.js
+++ mouse/entry/single.js
@@ -1,30 +1,58 @@
-var app = new GCApp();
+jsio('import timestep.ui.Console');
 
-var mainView = app.getView();
+var app = new GCApp({
+	/**
+	 * Draw an FPS counter in the top-left corner of the canvas
+	 */
+	showFPS: true,
+	
+	/**
+	 * Normally we repaint the screen every tick.  For this demo,
+	 * only repaint if an input event occurs (reduces CPU usage
+	 * for input-driven applications) or if view.needsRepaint()
+	 * is called on a subview.
+	 */
+	alwaysRepaint: false,
+	
+	/**
+	 * When creating higher-level input events, we normally fire 
+	 * input:move events even if the user is not generating them.
+	 * This is because if a view is moving/scaling, the user is
+	 * actually 'moving' relative to the coordinates of the view 
+	 * (from the view's frame of reference).  Normally this is
+	 * not noticeable, as it makes input events just 'work' as 
+	 * expected.  However, since we log on input:move events,
+	 * we don't want this behavior for this tutorial.
+	 */
+	continuousInputCheck: false
+});
 
-//onInputSelect is called whenever the view is clicked on.
-//pt is the x and y coordinates of the click
-mainView.onInputSelect = function(evt, pt) {
-	logger.log('Mouse clicked at ' + pt.x + ',' + pt.y);
-}
+mainView = app.getView();
 
-//onInputScroll is called whenver the mouse wheel is scrolled on
-//the view.  pt is the x and y coordinates of the mouse when it was 
-//scrolled.  evt.scrollDelta is the magnitude and direction of the scroll -
-// > 0 means up, < 0 means down.
+var console = new timestep.ui.Console({x: 25});
+logger.setListener(console); // where does logger go?
+mainView.addSubview(console);
+
+// onInputScroll is called whenver the mouse wheel is scrolled on
+// the view.  pt is the x and y coordinates of the mouse when it was 
+// scrolled.  evt.scrollDelta is the magnitude and direction of the scroll -
+//  > 0 means up, < 0 means down.
 mainView.onInputScroll = function(evt, pt) {
-	logger.log('Mouse wheel scrolled ' + (evt.scrollDelta > 0 ? 'up' : 'down'));
+	logger.log(pt.x + ',' + pt.y + ': scroll event ' + (evt.scrollDelta > 0 ? 'up' : 'down') + ' (' + evt.scrollDelta + ')');
 }
 
-
-//onInputMove is called whenver the mouse moves over the view
-mainView.onInputMove = function(evt, pt) {
-	//the point the mouse moved from
-	var srcPt = evt.srcPt;	
-}
-
-//onInputStart is called when a mouse click starts on a view.  It does not
-//wait for the click to be released like onInputSelect
+// onInputStart is called when a mouse click starts on a view.  It does not
+// wait for the click to be released like onInputSelect
 mainView.onInputStart = function(evt, pt) {
-
+	logger.log(pt.x + ',' + pt.y + ': "input:start" event'); //the point the mouse moved from
 }
+
+// onInputMove is called whenver the mouse moves over the view
+mainView.onInputMove = function(evt, pt) {
+	logger.log(pt.x + ',' + pt.y + ': "input:move" event'); //the point the mouse moved from
+}
+
+mainView.onInputSelect = function(evt, pt) {
+	logger.log(pt.x + ',' + pt.y + ': "input:select" event');
+}
+
