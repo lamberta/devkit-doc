80b1aed27cd18242541af69d9e121ce5f6843a2b
louisstow <louisstow@gmail.com>
2012-05-08 16:29:35 -0700

    * Image usage

---
+++ iceberg-api/Usage/shared/ImageUsage.js
@@ -0,0 +1,16 @@
+"use import";
+
+import timestep.View as View;
+import timestep.ImageView as ImageView;
+
+exports = Class(View, function(supr) {
+	this.init = function(opts) {
+		supr(this, "init", arguments);
+		
+		var myImage = new ImageView({
+			image: "resources/duck.png",
+			autoSize: true,
+			parent: this	
+		});
+	}
+});
