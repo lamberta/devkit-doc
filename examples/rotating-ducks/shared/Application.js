1e34084291232f68774c89713ed19733b83fee1c
Billy Lamberta <b@lamberta.org>
2012-07-09 18:17:42 -0700

    Changing the ducks example. Simple like, but not there yet.

--- examples/rotating-ducks/shared/Application.js
+++ examples/rotating-ducks/shared/Application.js
@@ -1,5 +1,3 @@
-"use import";
-
 import .util;
 
 import GC;
@@ -7,11 +5,8 @@ import timestep.TextView as TextView;
 import timestep.device as device;
 import timestep.ImageView as ImageView;
 
-import timestep.Sprite as Sprite;
-
 import timestep.Application as Application;
 
-//import GCDialogs.Dialog as Dialog;
 
 /**
  * 
@@ -25,140 +20,60 @@ exports = Class(GC.Application, function() {
 		alwaysRepaint: true
 	};
 
-	this.initUI = function() {
-
-		/*
-		var sprite = new Sprite({
-			parent: GC.app.view,
-			x: 0,
-			y: 50,
-			width: 34,
-			height: 86,
-			defaultAnimation: 'default',
-			animations: {
-				'default': {
-					imageURL: 'resources/img/character.png',
-					sheetWidth: 204,
-					sheetHeight: 344,
-					spritesWide: 6,
-					spritesHigh: 4,
-					start: 12,
-					end: 17
-					//width: 34,
-					//height: 86
-				}
-			}
-		});
-
-		//sprite.startAnimation('default');
-		
-		GLOBAL.s = sprite;
-
-		sprite.addAnimation('huh', {
-			imageURL: 'resources/img/character.png',
-			sheetWidth: 204,
-			sheetHeight: 344,
-			spritesWide: 6,
-			spritesHigh: 4,
-			start: 0,
-			end: 6
-		});
-
-		sprite.startAnimation('huh', {iterations: 2, callback: function () {
-			console.log('BOOM!');
-		}});
-		*/
+	this.initUI = function () {
 
 		this.view.style.backgroundColor = "#ffffff";
 
-		var views = [];
-
-
+		var ducks = [];
 		
-		
-		/*
-		for (var i = 100; i < 2450; i += 150) {
+		for (var i = 100, duck; i < 2450; i += 150) {
 			for (var j = 100; j < 1200; j += 150) {
-				views.push(new TextView({
-					parent: this.view,
-					text: "WTF?!",
-					color: "#ffffff",
-					backgroundColor: "#ff0000",
-					fontSize: 32,
-					width: 100,
-					height: 100,
-					r: 0.5,
-					//color: "#ff0000"
-					anchorX: 50,
-					anchorY: 50,
-					x: i,
-					y: j
-				}));
-			}
-		}*/
-
-		
-		for (var i = 100; i < 2450; i += 150) {
-			for (var j = 100; j < 1200; j += 150) {
-				views.push(new ImageView({
-					parent: this.view,
-					image: 'resources/img/duck.png',
-					//text: "WTF?!",
-					//color: "#ffffff",
-					//backgroundColor: "#ff0000",
-					//fontSize: 32,
-					width: 100,
-					height: 100,
-					r: 0.5,
-					//color: "#ff0000"
-					anchorX: 50,
-					anchorY: 50,
-					x: i,
-					y: j
-				}));
+				duck = new DuckView(i, j);
+				this.view.addSubview(duck);
+				ducks.push(duck);
 			}
 		}
-		
-		
-		GLOBAL.views = views;
 
-		/*
-		var iv = new ImageView({
-			parent: this.view,
-			image: 'resources/img/duck.png'
-		});
-		*/
-
-		//GLOBAL.iv = iv;
-		
-		/*
-		var d1 = new Dialog({
-			parent: this
-		});
-		*/
-		
-		//tv.setText('Hello, World!');
-
-		//this.view.addSubview(tv);
-
-		this.engine.subscribe('Tick', function () {
-			var i = views.length;
+		function rotate_ducks () {
+			var i = ducks.length;
 			while (i--) {
-				views[i].style.r += 0.1;
+				ducks[i].style.r += 0.1;
 			}
-		});
-	}
+		}
 
-	this.launchUI = function() {
+		GLOBAL.v0 = ducks[0];
 		
-	}
-
-	this.launchSinglePlayerGame = function() {
-		
-	}
-
-	this.launchMultiplayerGame = function() {
-		
+		this.engine.subscribe('Tick', rotate_ducks);
 	}
 });
 
+
+var DuckView = Class(ImageView, function (supr) {
+	
+	this.init = function (x, y) {
+
+		
+		
+		supr(this, 'init', [{
+			image: 'resources/img/duck.png',
+			x: x,
+			y: y,
+			width: 100,
+			height: 100,
+			r: 0.5,
+			anchorX: 50,
+			anchorY: 50
+		}]);
+
+		
+		//supr(this, 'init', duck_opts);
+		//console.log(x, y);
+		//console.log(this);
+		
+		//console.log(superClass);
+		
+		
+		//superClass.init.call(this, duck_opts);
+	};
+	
+});
