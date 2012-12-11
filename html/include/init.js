bc21a19a484197fb7a39280790ebfff7c247c488
Billy Lamberta <b@lamberta.org>
2012-12-11 13:42:38 -0800

    Update keyword index.

--- html/include/init.js
+++ html/include/init.js
@@ -2,23 +2,27 @@ $(function () {
 	/* dynamically set the max-height of the sidebar, since doing this in css is a pain.
 	 */
 	var sidebar = $('#toc-well'),
-			win = $(window);
-	function set_sidebar_height () {
-		if (win.width() < 979) {
-			sidebar.css({
-				'width': sidebar.parent().width(),
-				'max-height': 'none'
-			});
-		} else {
-			sidebar.css({
-				'width': sidebar.parent().width(),
-				'max-height': win.height() - sidebar.offset().top - 30
-			});
-		}
-	}
-	set_sidebar_height();
-	win.resize(set_sidebar_height);
+			win = $(window),
+			href_page = document.location.href.split('/').pop();
 
+	//index file doesn't have to deal with this nonsense
+	if (href_page !== 'index.html') {
+		function set_sidebar_height () {
+			if (win.width() < 979) {
+				sidebar.css({
+					'width': sidebar.parent().width(),
+					'max-height': 'none'
+				});
+			} else {
+				sidebar.css({
+					'width': sidebar.parent().width(),
+					'max-height': win.height() - sidebar.offset().top - 30
+				});
+			}
+		}
+		set_sidebar_height();
+		win.resize(set_sidebar_height);
+	}
 	
 	/* highlight section headers in sidebar
 	 */
@@ -37,7 +41,6 @@ $(function () {
 	
 	/* mark navbar links as active if on the same page
 	 */
-	var href_page = document.location.href.split('/').pop();
 	$('.navbar-inner a').each(function () {
 		var a_page = this.href.split('/').pop();
 		if (a_page === href_page) {
@@ -74,46 +77,55 @@ $(function () {
 /* Keywords for the search box and the urls they resolve to.
  */
 var keyword_index = {
-	'Application': 'api/appengine.html',
-	'GC.app': 'api/appengine.html#singleton-gc.app',
-	'Engine': 'api/appengine.html#class-ui.engine',
-	'GC.app.engine': 'api/appengine.html#singleton-gc.app.engine',
-	'Events': 'api/event.html',
-	'Emitter': 'api/event.html#class-event.emitter',
-	'Callback': 'api/event.html#class-event.callback',
-	'InputEvent': 'api/event.html#class-event.input.inputevent',
-	'Filters': 'api/ui-filter.html',
-	'LinearAddFilter': 'api/ui-filter.html#class-ui.filter.linearaddfilter',
-	'TintFilter': 'api/ui-filter.html#class-ui.filter.tintfilter',
-	'MultiplyFilter': 'api/ui-filter.html#class-ui.filter.multiplyfilter',
-	'PositiveMaskFilter': 'api/ui-filter.html#class-ui.filter.positivemaskfilter',
-	'NegativeMaskFilter': 'api/ui-filter.html#class-ui.filter.negativemaskfilter',
-	'Image': 'api/ui-imageview.html#class-ui.resource.image',
-	'ImageView': 'api/ui-imageview.html#class-ui.imageview',
-	'ImageScaleView': 'api/ui-imageview.html#class-ui.imagescaleview',
-	'ScrollView': 'api/ui-scrollview.html',
-	'SpriteView': 'api/ui-spriteview.html',
-	'StackView': 'api/ui-stackview.html',
-	'text': 'api/ui-text.html',
-	'Font': 'api/ui-text.html#class-ui.resource.font',
-	'TextInputView': 'api/ui-text.html#class-ui.textinputview',
-	'TextPromptView': 'api/ui-text.html#class-ui.textpromptview',
-	'TextView': 'api/ui-text.html#class-ui.textview',
-	'View': 'api/ui-view.html',
-	'ButtonView': 'api/ui-widget-buttonview.html',
-	'CellView': 'api/ui-widget-listview.html#class-ui.widget.cellview',
-	'DataSource': 'api/ui-widget-listview.html#class-gcdatasource',
-	'ListView': 'api/ui-widget-listview.html#class-ui.widget.listview',
-	'Spinner': 'api/ui-widget-spinner.html',
-	'device': 'api/device.html',
-	'animate': 'api/animate.html',
-	'Animator': 'api/animate.html#class-animate.animator',
-	'Animator Group': 'api/animate.html#class-group',
-	'Audio': 'api/audio.html',
-	'AudioManager': 'api/audio.html#class-audiomanager',
-	'Utlities': 'api/utilities.html',
-	'Color': 'api/color.html',
-	'Math': 'api/math.html'
+	"GC.Application": "api/appengine.html",
+	"GC.app": "api/appengine.html#singleton-gc.app",
+	"ui.Engine": "api/appengine.html#class-ui.engine",
+	"GC.app.engine": "api/appengine.html#singleton-gc.app.engine",
+	"events": "api/event.html",
+	"event.Emitter": "api/event.html#class-event.emitter",
+	"event.Callback": "api/event.html#class-event.callback",
+	"event.input.InputEvent": "api/event.html#class-event.input.inputevent",
+	"filters": "api/ui-filter.html",
+	"ui.filter.Filter": "api/ui-filter.html#class-ui.filter.filter",
+	"ui.filter.LinearAddFilter": "api/ui-filter.html#class-ui.filter.linearaddfilter",
+	"ui.filter.TintFilter": "api/ui-filter.html#class-ui.filter.tintfilter",
+	"ui.filter.MultiplyFilter": "api/ui-filter.html#class-ui.filter.multiplyfilter",
+	"ui.filter.PositiveMaskFilter": "api/ui-filter.html#class-ui.filter.positivemaskfilter",
+	"ui.filter.NegativeMaskFilter": "api/ui-filter.html#class-ui.filter.negativemaskfilter",
+	"ui.ImageView": "api/ui-imageview.html#class-ui.imageview",
+	"ui.ImageScaleView": "api/ui-imageview.html#class-ui.imagescaleview",
+	"ui.resource.Image": "api/ui-imageview.html#class-ui.resource.image",
+	"ui.ScrollView": "api/ui-scrollview.html",
+	"ui.SpriteView": "api/ui-spriteview.html",
+	"ui.StackView": "api/ui-stackview.html",
+	"text": "api/ui-text.html",
+	"ui.TextInputView": "api/ui-text.html#class-ui.textinputview",
+	"ui.TextPromptView": "api/ui-text.html#class-ui.textpromptview",
+	"ui.TextView": "api/ui-text.html#class-ui.textview",
+	"ui.resource.Font": "api/ui-text.html#class-ui.resource.font",
+	"ui.View": "api/ui-view.html",
+	"ui.widget.ButtonView": "api/ui-widget-buttonview.html",
+	"ui.widget.ListView": "api/ui-widget-listview.html#class-ui.widget.listview",
+	"ui.widget.CellView": "api/ui-widget-listview.html#class-ui.widget.cellview",
+	"DataSource": "api/ui-widget-listview.html#class-gcdatasource",
+	"ui.widget.Spinner": "api/ui-widget-spinner.html",
+	"device": "api/device.html",
+	"animate": "api/animate.html",
+	"animate.Animator": "api/animate.html#class-animate.animator",
+	"animate.Group": "api/animate.html#class-group",
+	"Audio": "api/audio.html",
+	"AudioManager": "api/audio.html#class-audiomanager",
+	"Utilities": "api/utilities.html",
+	"import": "api/utilities.html#import",
+	"Class": "api/utilities.html#class-name-superconstructor-constructor",
+	"bind": "api/utilities.html#bind-thisarg-callback-args",
+	"merge": "api/utilities.html#merge-obj1-obj2-obj3",
+	"GLOBAL": "api/utilities.html#global",
+	"Color": "api/color.html",
+	"Math": "api/math.html",
+	"math.util": "api/math.html#module-math.util",
+	"math.array": "api/math.html#module-math.array",
+	"math.geom.Point": "api/math.html#class-math.geom.point"
 };
 
 
