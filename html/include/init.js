ebbbc5d4d8570e1cb78aad190dcbd7079b005e66
Billy Lamberta <b@lamberta.org>
2013-01-26 15:08:14 -0800

    Remove analytics.

--- html/include/init.js
+++ html/include/init.js
@@ -242,11 +242,3 @@ var keyword_index = {
   "math.array": "api/math.html#module-math.array",
   "math.geom.Point": "api/math.html#class-math.geom.point"
 };
-
-
-/* google analytics
- */
-var _gaq=[['_setAccount','UA-36886915-1'],['_trackPageview']];
-(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-s.parentNode.insertBefore(g,s)}(document,'script'));
